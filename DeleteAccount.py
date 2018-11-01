f95bb9d10c6d331da7be56a530d01e5828dbcd98
john3885 <john3885@uwm.edu>
2018-10-31 22:38:42 -0500

    Login/Logout features added

--- DeleteAccount.py
+++ DeleteAccount.py
@@ -6,5 +6,9 @@ class DeleteAccount:
         args is a list containing the following:
             ["delete_account", username]
     """
-    def action(self, args):
+    def action(self, args, user):
+        if user is None:
+            "Print you must be logged in to perform this action."
+            return None
+
         print("Delete Account command entered")
--- Login.py
+++ Login.py
@@ -1,10 +1,34 @@
+import TextFileInterface
+import User
+import hashlib
+
 class Login:
-    def __init__(self):
-        pass
+
+    def __init__(self, database):
+        self.database = database
 
     """
         args is a list containing the following:
-            ["login", username, password]
+            ["login", <username>, <password>]
     """
-    def action(self, args):
-        print("Login Command Entered")
+    def action(self, args, user):
+        if user is not None:
+            print("Someone else is logged in.")
+            return user
+
+        user_list = self.database.get_accounts()
+
+        for i in user_list:
+            if i["name"] is args[1]:
+                user = i
+
+        hash = hashlib.new("md5")
+        hash.update(b"args[2]")
+        hashed_password = hash.hexdigest()
+        if user["password"] != hashed_password:
+            print("User name or Password is Incorrect")
+            return None
+
+        self.database.set_logged_in(user["username"])
+        print("Logged in successfully")
+        return User.User(user["username"], user["role"])
