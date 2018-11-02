c70c0bad3f88fdf1ee919ae3e53141668e9c5670
Kylebunzel <44423191+Kylebunzel@users.noreply.github.com>
2018-11-01 20:49:27 -0500

    Update CreateCourse.py
    Manually edited this so I don't break stuff.

--- CreateCourse.py
+++ CreateCourse.py
@@ -2,15 +2,29 @@ import Command
 
 
 class CreateCourse(Command.Command):
-    def __init__(self):
-        pass
+    def __init__(self, database):
+        self.database = database
 
     """
         args is a list containing the following:
             ["create_Course", course_number, course_name,]
     """
-    def action(self, args):
-        print("Create Course command entered.")
-        """
-            Creates a course with specified name and number.
-        """
+    def action(self, args, user):
+        if user is None:
+            print("You must be logged in to perform this action.")
+            return None
+
+        if user.get_role() not in ["supervisor", "admin"]:
+            print("Permission Denied")
+            return user
+
+        if len(args) != 2:
+            print("Invalid Arguments")
+            return user
+
+        self.database.create_course(args[1], args[2],)
+
+        print("Course Created Successfully")
+        return None
+
+
