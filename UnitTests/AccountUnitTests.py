d2d58ccffde1d5b952388eb5be9f149b88bc4459
adkanyer <43828330+adkanyer@users.noreply.github.com>
2018-11-03 20:41:04 -0500

    Fixed LoginUnitTests and LogoutUnitTests

---
+++ UnitTests/AccountUnitTests.py
@@ -0,0 +1,15 @@
+import unittest
+from TextFileInterface import TextFileInterface
+from Components.AccountCommands import CreateAccount, DeleteAccount
+from Environment import Environment
+
+
+class Create_Account_Unit_Tests(unittest.TestCase):
+
+    def setUp(self):
+        self.environment = Environment(TextFileInterface())
+
+    def test_create_account_no_args(self):
+        ca = CreateAccount(self.environment)
+        self.assertEqual(1, 1)
+
---
+++ UnitTests/LoginUnitTests.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+from Components.Login import Login
+from TextFileInterface import TextFileInterface
+from Environment import Environment
+from User import User
+
+
+class LoginUnitTests(TestCase):
+    def setUp(self):
+        tfi = TextFileInterface(relative_directory="TestDB/")
+        self.environment = Environment(tfi)
+        self.environment.database.clear_database()
+        self.environment.database.create_account("root", "root", "administrator")
+        self.environment.database.set_logged_in("root")
+
+    def test_not_enough_args(self):
+        login = Login(self.environment)
+        login.action(["login", "testuser"])
+        self.assertIsNone(self.environment.user)
+
+    def test_too_many_args(self):
+        login = Login(self.environment)
+        login.action(["login", "testuser", "1234", "foo"])
+        self.assertIsNone(self.environment.user)
+
+    def test_already_logged_in(self):
+        self.environment.database.create_account("testuser", "1234", "supervisor")
+        login = Login(self.environment)
+        login.action(["login", "testuser", "1234"])
+        self.assertIsNotNone(self.environment.user)
+
+    def test_wrong_username(self):
+        self.environment.database.create_account("testuser", "1234", "supervisor")
+        login = Login(self.environment)
+        login.action(["login", "testuse", "1234"])
+        self.assertIsNone(self.environment.user)
+
+    def test_wrong_password(self):
+        self.environment.database.create_account("testuser", "1234", "supervisor")
+        login = Login(self.environment)
+        login.action(["login", "testuser", "4321"])
+        self.assertIsNone(self.environment.user)
+
+    def test_wrong_both(self):
+        self.environment.database.create_account("testuser", "1234", "supervisor")
+        login = Login(self.environment)
+        login.action(["login", "testuse", "123"])
+        self.assertIsNone(self.environment.user)
+
+    def test_all_correct(self):
+        self.environment.database.create_account("testuser", "1234", "supervisor")
+        login = Login(self.environment)
+        login.action(["login", "testuser", "1234"])
+        self.assertIsNotNone(self.environment.user)
